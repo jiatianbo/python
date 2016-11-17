56eccba0f10afcfc03ce87c9ed8ce6a12f084ca0
jiatianbo <jiatianbo@126.com>
2016-11-17 21:49:45 +0800

    add in fxt1.py

---
+++ fxt1.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+
+def capstr(name):
+    return name.capitalize()
+
+assert capstr('lilei') == 'Lilei'
+assert capstr('hanmeimei') =='Hanmeimei'
+assert capstr('jiatianbo') == 'Jiatianbo'
